df66c2ea387708d1b82ea0121f2dc43e7e8fe28b
Unknown <jme919@gmail.com>
2018-05-03 18:07:54 -0400

    created all of the necessary files

---
+++ db/seed.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gourdon-Hamsey Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Poutine on the Ritz Burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Never leave home without your cardigan.', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beets of Burden Burger', False );
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chile Relleno-You-Didn't Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Curd-fect Strangers Burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeses Is Born Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jingle Bell Pepper Burger", true);
+
+
+
+
