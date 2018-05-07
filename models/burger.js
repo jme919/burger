367ef0d13238464b080367c3d836ff2c24fee1a1
Unknown <jme919@gmail.com>
2018-05-07 19:04:07 -0400

    update some function names

--- models/burger.js
+++ models/burger.js
@@ -2,28 +2,21 @@
 var orm = require("../config/orm.js");
 
 var burgersFun = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  dislplayAll: function(cb) {
+    orm.displayAll("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  addBurger: function(burgerName, cb) {
+    orm.addBurger(burgerName, function(res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+  updateDev: function(dev, id,  cb) {
+    orm.updateDev(dev, id,  function(res) {
       cb(res);
     });
-  },
-  delete: function(condition, cb) {
-    orm.delete("", condition, function(res) {
-      cb(res);
-    });
-  }
-};
-
+ 
 // Export the database functions for the controller Controller.js).
 module.exports = burgersFun;
